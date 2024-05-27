0f561826d87286535decd6a4320efe3fc450c24a
maximiliano.farias-v <maximiliano.farias-v@n5now.com>
2024-05-27 17:43:45 -0300

    se agregan cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors());
 
 const productos = [
     {
